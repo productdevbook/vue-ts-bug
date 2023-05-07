32aff8511c614d130faec50b3fd4804084bc45c9
productdevbook <hi@productdevbook.com>
2023-05-07 08:51:05 +0300

    init

---
+++ src/aspect-ratio.ts
@@ -0,0 +1,65 @@
+import type { ComponentPublicInstance } from 'vue'
+import { computed, defineComponent, h, ref } from 'vue'
+import type { ComponentPropsWithoutRef, ElementRef } from './primitive'
+import { Primitive } from './primitive'
+
+type PrimitiveAspectRatioProps = ComponentPropsWithoutRef<typeof Primitive.div>
+type AspectRatioElement = ElementRef<typeof Primitive.div>
+
+interface AspectRatioProps extends PrimitiveAspectRatioProps {
+  ratio?: number
+}
+const NAME = 'AspectRatio'
+
+const AspectRatio = defineComponent({
+  name: NAME,
+  inheritAttrs: false,
+  setup(props, { attrs, slots, expose }) {
+    const { ratio = 1 / 1, style, ...aspectRatioProps } = attrs as AspectRatioProps
+    const innerRef = ref<ComponentPublicInstance>()
+
+    expose({
+      innerRef: computed(() => innerRef.value?.$el),
+    })
+
+    const originalReturn = () => h(
+      'div', {
+        'style': {
+          position: 'relative',
+          width: '100%',
+          paddingBottom: `${100 / ratio}%`,
+        },
+        'data-radix-aspect-ratio-wrapper': '',
+      },
+      [
+        h(
+          Primitive.div,
+          {
+            ...aspectRatioProps,
+            ref: innerRef,
+            style: {
+              ...(style as any),
+              position: 'absolute',
+              top: 0,
+              right: 0,
+              bottom: 0,
+              left: 0,
+            },
+          },
+          slots.default && slots.default(),
+        ),
+      ],
+    )
+
+    return originalReturn as unknown as {
+      innerRef: AspectRatioElement
+    }
+  },
+})
+
+const OkuAspectRatio = AspectRatio as typeof AspectRatio & (new () => { $props: AspectRatioProps })
+
+type OkuAspectRatioElement = Omit<InstanceType<typeof AspectRatio>, keyof ComponentPublicInstance>
+
+export { OkuAspectRatio }
+export type { AspectRatioProps, OkuAspectRatioElement }
---
+++ src/primitive.ts
@@ -0,0 +1,106 @@
+import type { DefineComponent, FunctionalComponent, VNodeRef } from 'vue'
+import { defineComponent, h, onMounted } from 'vue'
+
+/* -------------------------------------------------------------------------------------------------
+ * Primitive
+ * ----------------------------------------------------------------------------------------------- */
+const NODES = [
+  'a',
+  'button',
+  'div',
+  'form',
+  'h2',
+  'h3',
+  'img',
+  'input',
+  'label',
+  'li',
+  'nav',
+  'ol',
+  'p',
+  'span',
+  'svg',
+  'ul',
+] as const
+
+type ElementConstructor<P> =
+  | (new () => { $props: P })
+  | ((props: P, ...args: any) => FunctionalComponent<any, any>)
+
+//  extends keyof JSX.IntrinsicElements | ElementConstructor<any>
+type ComponentProps<T extends keyof JSX.IntrinsicElements | ElementConstructor<any>> =
+  T extends ElementConstructor<infer P>
+    ? P
+    : T extends keyof JSX.IntrinsicElements
+      ? JSX.IntrinsicElements[T]
+      : {}
+
+ type ComponentType<T = {}> = DefineComponent<T> | FunctionalComponent<T>
+
+ type ComponentPropsWithoutRef<T extends ElementType> = PropsWithoutRef<
+  ComponentProps<T>
+>
+
+ type ElementType<P = any> = {
+   [K in keyof JSX.IntrinsicElements]: P extends JSX.IntrinsicElements[K] ? K : never
+ }[keyof JSX.IntrinsicElements] |
+ ComponentType<P>
+
+type PropsWithoutRef<P> = P extends object ? {
+  [K in keyof P as K extends 'ref' ? never : K]: P[K]
+} : P
+
+type PropsWithRef<P> =
+  'ref' extends keyof P
+    ? P extends { ref?: infer R | undefined }
+      ? string extends R
+        // TODO: ref dynamic `IntrinsicElements` support
+        ? PropsWithoutRef<P> & { ref?: VNodeRef | undefined }
+        : any
+      : any
+    : any
+
+type VueComponentPropsWithRef<T extends ElementType> =
+     T extends new () => { $props: infer P }
+       // TODO: ref dynamic `IntrinsicElements` support, `VNodeRef` dont support T
+       ? PropsWithoutRef<P> & { ref?: VNodeRef | undefined }
+       : PropsWithRef<ComponentProps<T>>
+
+type Primitives = { [E in typeof NODES[number]]: PrimitiveDefineComponent<E> }
+
+type PrimitivePropsWithRef<E extends ElementType> = VueComponentPropsWithRef<E> & {
+  asChild?: boolean
+}
+
+type ElementRef<T extends keyof JSX.IntrinsicElements | ElementConstructor<any>> =
+   T extends PrimitiveDefineComponent<infer E>
+     ? E extends keyof JSX.IntrinsicElements
+       ? JSX.IntrinsicElements[E]
+       : never
+     : never
+
+interface PrimitiveDefineComponent<E extends ElementType> extends DefineComponent<PrimitivePropsWithRef<E>> {}
+
+const Primitive = NODES.reduce((primitive, node) => {
+  const Node = defineComponent<PrimitivePropsWithRef<typeof node>>({
+    name: `Primitive${node}`,
+    inheritAttrs: false,
+    setup(props, { attrs, slots }) {
+      onMounted(() => {
+        (window as any)[Symbol.for('oku-ui')] = true
+      })
+      const Tag: any = props.asChild ? 'slot' : node
+
+      return () => props.asChild ? slots.default?.() : h(Tag, { ...attrs }, slots.default?.())
+    },
+  })
+  return { ...primitive, [node]: Node }
+}, {} as Primitives)
+
+const OkuPrimitive = Primitive
+
+export {
+  OkuPrimitive,
+  Primitive,
+}
+export type { ComponentPropsWithoutRef, ElementRef }
